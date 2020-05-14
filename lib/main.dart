d35e9ecb1687e0e091626cc3c238330e0415e756
Ben O'neill <boneill@Bens-MacBook-Pro.local>
2020-05-14 07:50:03 -0500

    gotta go to work

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+//    Icon(Icons.check, color: Colors.green),
+//    Icon(Icons.close, color: Colors.red),
+//    Icon(Icons.close, color: Colors.red),
+//    Icon(Icons.close, color: Colors.red),
+//    Icon(Icons.close, color: Colors.red),
+//    Icon(Icons.close, color: Colors.red)
+  ];
+
+  List<String> questions = [
+     'You can lead a cow down stairs but not up stairs.',
+     'Approximately one quarter of human bones are in the feet.',
+     'A slug\'s blood is green.'
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -32,12 +49,12 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          flex: 5,
+          flex: 5, // whats this
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +79,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
@@ -80,13 +100,18 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  questionNumber++;
+                });
+
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
-        //TODO; boom boom
-      ],
+        Row(
+          children: scoreKeeper,
+        ),
+      ], //parent column widget list
     );
   }
 }
