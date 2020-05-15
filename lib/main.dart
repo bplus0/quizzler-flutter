421acfcb541fbda2bdd60dace6fa84963ca69b26
Ben O'neill <boneill@Bens-MacBook-Pro.local>
2020-05-14 20:50:27 -0500

    we have a class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -34,11 +35,28 @@ class _QuizPageState extends State<QuizPage> {
 //    Icon(Icons.close, color: Colors.red)
   ];
 
-  List<String> questions = [
-     'You can lead a cow down stairs but not up stairs.',
-     'Approximately one quarter of human bones are in the feet.',
-     'A slug\'s blood is green.'
+//  List<String> questions = [
+//    'You can lead a cow down stairs but not up stairs.',
+//    'Approximately one quarter of human bones are in the feet.',
+//    'A slug\'s blood is green.'
+//  ];
+//
+//  List<bool> answers = [false, true, true];
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
   ];
+
+//  Question q1 = new Question(
+//      q: 'You can lead a cow down stairs but not up stairs.', a: false);
+//
+//  Question q2 = new Question(
+//      q: 'Approximately one quarter of human bones are in the feet.', a: true);
+//
+//  Question q3 = new Question(
+//      q: 'A slug\'s blood is green.', a: true);
 
   int questionNumber = 0;
 
@@ -54,7 +72,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -79,6 +97,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctanswer = questionBank[questionNumber].questionAnswer;
+
+                if (correctanswer == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+
                 setState(() {
                   questionNumber++;
                 });
@@ -100,10 +126,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctanswer = questionBank[questionNumber].questionAnswer;
+
+                if (correctanswer == false) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+
                 setState(() {
                   questionNumber++;
                 });
-
               },
             ),
           ),
